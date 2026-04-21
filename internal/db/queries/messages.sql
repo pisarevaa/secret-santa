095f9206c36d423a923134f7c74e993ce8a1adde
Andrey Pisarev <andrpisarev@gmail.com>
2026-04-21 08:37:42 +0300

    refactor: удалить неиспользуемые ListMessages/ListMessagesBefore (M-3)
    Сгенерированные sqlc запросы нигде не вызывались. Используется только
    ListChatMessages.

--- internal/db/queries/messages.sql
+++ internal/db/queries/messages.sql
@@ -1,18 +1,6 @@
 -- name: CreateMessage :one
 INSERT INTO messages (group_id, sender_id, recipient_id, direction, body) VALUES (?, ?, ?, ?, ?) RETURNING *;
 
--- name: ListMessages :many
-SELECT * FROM messages
-WHERE group_id = ? AND sender_id = ? AND recipient_id = ? AND direction = ?
-ORDER BY created_at DESC
-LIMIT 50;
-
--- name: ListMessagesBefore :many
-SELECT * FROM messages
-WHERE group_id = ? AND sender_id = ? AND recipient_id = ? AND direction = ? AND id < ?
-ORDER BY created_at DESC
-LIMIT 50;
-
 -- name: ListChatMessages :many
 SELECT * FROM messages
 WHERE group_id = ?
